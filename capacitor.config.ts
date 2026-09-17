7e20bcb1ba17e953ceea9f680f3574d6cd888db4
codysowa <cody.sowa@outlook.com>
2026-09-17 15:55:46 -0700

    Add Capacitor iOS configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'com.beautifulgameiq.app',
+  appName: 'Beautiful Game IQ',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+}
+
+export default config
